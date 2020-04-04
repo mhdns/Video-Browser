1dadf7631cc872e25c35bc3e5357f0c1ad0a63af
Muhammad Anas <muhd.anas@outlook.com>
2020-04-04 15:20:38 +0800

    Added VideoItems component.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
+import './App.css';
 import React from 'react';
 import SearchBar from './components/search/SearchBar';
 import SearchState from './context/SearchState';
+import VideoList from './components/video/VideoList';
 
 const App = () => (
   <SearchState>
     <div className="ui container">
       <SearchBar />
+      <VideoList />
     </div>
   </SearchState>
 );
