dba51edc9ab1bb28617d13e161e1450b18d0b419
Muhammad Anas <muhd.anas@outlook.com>
2020-04-04 16:29:33 +0800

    Added styling to see VideoDetail and VideoList side by side.

--- src/App.js
+++ src/App.js
@@ -3,12 +3,23 @@ import React from 'react';
 import SearchBar from './components/search/SearchBar';
 import SearchState from './context/SearchState';
 import VideoList from './components/video/VideoList';
+import VideoDetail from './components/video/VideoDetail';
 
 const App = () => (
   <SearchState>
     <div className="ui container">
       <SearchBar />
-      <VideoList />
+      <div className="ui grid">
+        <div className="ui row">
+          <div className="eleven wide column">
+            <VideoDetail />
+          </div>
+          <div className="five wide column">
+            <VideoList />
+          </div>
+        </div>
+      </div>
+
     </div>
   </SearchState>
 );
